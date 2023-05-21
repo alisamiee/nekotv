ffa06ef00159bddae9f402211e8ad62257663629
Ali Samiee <99135990+AliSamiee@users.noreply.github.com>
2023-05-21 22:14:13 +0330

    feat: add card gallery

---
+++ src/components/CardGallery.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card-gallery">
+        <h3>{{ galleryName }}</h3>
+        <div class="cards">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+import CardItem from './CardItem.vue';
+
+export default {
+    components: {
+        CardItem
+    },
+    props: {
+        galleryName: String
+    },
+};
+</script>
+
+<style scoped>
+.card-gallery {
+    width: 100%;
+}
+
+.card-gallery h3 {
+    font-weight: 300;
+    font-size: 2.5rem;
+    margin: 1.3rem auto;
+}
+
+.cards {
+    overflow: hidden;
+    display: flex;
+    flex-wrap: wrap;
+}
+</style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="card-holder">
+        <div class="card">
+            <div class="image">
+                <img :src="imgUrl" :alt="cardTitle" />
+                <span class="episodes">{{ episodeCount }}</span>
+            </div>
+            <div class="card-title">{{ cardTitle }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        imgUrl: {
+            type: String,
+            required: true
+        },
+        cardTitle: {
+            type: String,
+            required: true
+        },
+        episodeCount: Number,
+    }
+};
+</script>
+
+<style scoped>
+.card {
+    width: 100%;
+    background-color: var(--color-surface);
+    border-radius: 20px;
+    overflow: hidden;
+}
+
+.image {
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+    padding-bottom: 140%;
+}
+
+.image img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.episodes {
+    position: absolute;
+    bottom: 2px;
+    right: 2px;
+    padding: 0.2rem 0.7rem;
+    background-color: var(--color-accent);
+    border-radius: 20px;
+    font-size: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-title {
+    height: 3rem;
+    margin: auto 0.7rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    font-weight: 700;
+    font-size: 1rem;
+}
+
+.card-holder {
+    display: flex;
+    width: 16.6666666667%;
+    padding: 0 8px;
+    margin-bottom: 16px;
+}
+
+@media (max-width: 1000px) {
+    .card-holder {
+        width: 20%;
+    }
+}
+
+@media (max-width: 768px) {
+    .card-holder {
+        width: 33.3333333333%;
+    }
+}
+
+@media (max-width: 425px) {
+    .card-holder {
+        width: 50%;
+    }
+}
+
+@media (max-width: 1200px) {
+    .card-title {
+        font-size: 0.8rem;
+    }
+}
+</style>
