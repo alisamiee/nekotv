90f2c54956c59cf440035d601b88cba67e1f4c12
Ali Samiee <99135990+AliSamiee@users.noreply.github.com>
2023-05-23 01:05:08 +0330

    feat: update InfoStore

--- src/stores/InfoStore.js
+++ src/stores/InfoStore.js
@@ -4,5 +4,10 @@ import showInfo from '@/assets/showinfo.json';
 export const useInfoStore = defineStore('info', {
     state: () => ({
         shows: showInfo
-    })
+    }),
+    getters: {
+        getShow: (state) => {
+            return (showId) => state.shows.find(({ id }) => id === showId);
+        }
+    }
 });
