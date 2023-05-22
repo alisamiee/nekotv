e3f3cd3decf6cf67ae8bbdc73eee1d7211f215a1
Ali Samiee <99135990+AliSamiee@users.noreply.github.com>
2023-05-22 20:37:25 +0330

    feat: add WatchView to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import WatchView from '../views/WatchView.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -8,6 +9,12 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: HomeView
+        },
+        {
+            path: '/watch/:id',
+            name: 'watch',
+            component: WatchView,
+            props: true
         }
     ]
 });
