b21681489a2bc4b0c05558e6dae415fa082e6541
Ali Samiee <99135990+AliSamiee@users.noreply.github.com>
2023-05-23 11:09:15 +0330

    feat: update navbar styles

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -6,10 +6,15 @@
                 <h1>NekoTV</h1>
             </router-link>
             <div>
-                <IconSearch class="icon" />
-                <IconBell class="icon" />
-                <IconSun @click="toggleTheme" class="icon" />
-                <IconLogin class="icon" />
+                <div class="search-container">
+                    <input type="text" class="search-input">
+                    <IconSearch class="icon" />
+                </div>
+                <!-- <IconBell class="icon" /> -->
+                <div class="other-icons">
+                    <IconSun @click="toggleTheme" class="icon" />
+                    <IconLogin class="icon" />
+                </div>
             </div>
         </nav>
     </header>
@@ -92,6 +97,10 @@ a {
     margin-bottom: -0.5rem;
 }
 
+.icon {
+    height: 1.8rem;
+}
+
 nav>div,
 .brand {
     display: flex;
@@ -107,9 +116,57 @@ nav>div,
     cursor: pointer;
 }
 
+.search-container {
+    display: inline-flex;
+    justify-content: flex-end;
+    gap: 8px;
+    align-items: center;
+    position: relative;
+    height: 100%;
+    margin-left: .5rem;
+}
+
+.search-input {
+    display: inline-block;
+    border-radius: 20px;
+    width: 0;
+    height: 2rem;
+    background-color: var(--color-surface);
+    outline: none;
+    border: 0 solid transparent;
+    transition: all 0.5s ease-in-out;
+}
+
+.search-container:hover .search-input,
+.search-input:focus {
+    width: 80%;
+    border: 1px solid var(--color-white);
+    color: var(--color-white);
+    padding-left: .8rem;
+    padding: 0 0.8rem;
+}
+
+
+.search-container + div {
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+}
+
+
 @media (max-width: 425px) {
     nav {
         width: 99%;
     } 
+
+    .brand h1 {
+        display: none;
+    }
+
+    .search-container:hover + div,
+    nav:has(.search-input:focus) .other-icons {
+        display: none;
+    }
+
 }
 </style>
